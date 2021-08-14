106f6d425ba930f6919d2be6cd7ce5e29fe49bcd
saahil <saahilm30@gmail.com>
2021-08-13 23:12:13 -0500

    Updated reminders.dart

--- lib/reminders.dart
+++ lib/reminders.dart
@@ -4,89 +4,184 @@ class Reminders extends StatefulWidget {
   @override
   _RemindersState createState() => _RemindersState();
 }
+class HeroDialogRoute<T> extends PageRoute<T> {
+  /// {@macro hero_dialog_route}
+  HeroDialogRoute({
+    required WidgetBuilder builder,
+    RouteSettings? settings,
+    bool fullscreenDialog = false,
+  })  : _builder = builder,
+        super(settings: settings, fullscreenDialog: fullscreenDialog);
 
-class _RemindersState extends State<Reminders> {
-  TextEditingController customController = TextEditingController();
-  late String valueText = '';
-  late String codeDialog = '';
-  createAlertDialog(BuildContext context){
-    return showDialog(context: context, builder: (context){
-      return AlertDialog(
-        title: Text(
-          'Reminder:'),
-        content: TextField(
-          onChanged: (value) {
-            setState(() {
-              valueText = value;
-            });
-          },
-          controller: customController,
-        ),
+  final WidgetBuilder _builder;
 
-        actions: <Widget>[
-          /*ElevatedButton(onPressed: () {},
-              style: ElevatedButton.styleFrom(
-              primary: Colors.green,
-              textStyle: TextStyle(fontSize: 15)
-          ),child: Text('Submit')),*/
-          MaterialButton(child: Text('Go Back', style: TextStyle(fontSize: 16)),onPressed: (){
-            Navigator.of(context).pop();
-          },),
-          MaterialButton(child: Text('Submit', style: TextStyle(fontSize: 16)),onPressed: (){
-            codeDialog = valueText;
-            Navigator.of(context).pop(customController.text);
-            },),
-        ],
-      );
-    });
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissble => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color get barrierColor => Colors.black54;
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
   }
 
   @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return _builder(context);
+  }
+
+  @override
+  String get barrierLabel => 'Popup dialog open';
+}
+
+abstract class AppColors {
+  /// Dark background color.
+  static const Color backgroundColor = Color(0xFF191D1F);
+
+  /// Slightly lighter version of [backgroundColor].
+  static const Color backgroundFadedColor = Color(0xFF191B1C);
+
+  /// Color used for cards and surfaces.
+  static const Color cardColor = Color(0xFF1F2426);
+
+  /// Accent color used in the application.
+  static const Color accentColor = Color(0xFFef8354);
+}
+class _RemindersState extends State<Reminders> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text(''),
-          Text(''),
-          Text(''),
-          Text(''),
-          Flexible(
-            child: FractionallySizedBox(
-              heightFactor: 0.9
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(HeroDialogRoute(builder: (context) {
+            return const _AddTodoPopupCard(key: null,);
+          }));
+        },
+        child: Hero(
+          tag: _heroAddTodo,
+
+
+          child: Material(
+            color: AppColors.accentColor,
+            elevation: 2,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+            child: const Icon(
+              Icons.add_rounded,
+              size: 56,
             ),
-          ),
-          Text(valueText),
-          Flexible(
-            child: FractionallySizedBox(
-              heightFactor: 1,
-            ),
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: FractionallySizedBox(
-                  widthFactor: 0.9,
-                ),
-              ),
-              ElevatedButton.icon(
-                label: Text('Add'),
-                onPressed: () {
-                  createAlertDialog(context).then((value){
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.green,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    textStyle: TextStyle(
-                      fontSize: 20,
-                    )
-                ),
-                icon: Icon(Icons.add),
-              )
-            ],
+
           )
-        ],
+        ),
       ),
     );
   }
 }
+
+const String _heroAddTodo = 'add-todo-hero';
+
+/// {@template add_todo_popup_card}
+/// Popup card to add a new [Todo]. Should be used in conjuction with
+/// [HeroDialogRoute] to achieve the popup effect.
+///
+/// Uses a [Hero] with tag [_heroAddTodo].
+/// {@endtemplate}
+class _AddTodoPopupCard extends StatelessWidget {
+  /// {@macro add_todo_popup_card}
+  const _AddTodoPopupCard({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Hero(
+          tag: _heroAddTodo,
+          //createRectTween: (begin, end) {
+           // return CustomRectTween(begin: begin, end: end);
+          //},
+          child: Material(
+            color: AppColors.accentColor,
+            elevation: 2,
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'New todo',
+                        border: InputBorder.none,
+                      ),
+                      cursorColor: Colors.white,
+                    ),
+                    const Divider(
+                      color: Colors.white,
+                      thickness: 0.2,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Write a note',
+                        border: InputBorder.none,
+                      ),
+                      cursorColor: Colors.white,
+                      maxLines: 6,
+                    ),
+                    const Divider(
+                      color: Colors.white,
+                      thickness: 0.2,
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: const Text('Add'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ConstrainedView extends StatelessWidget {
+  const ConstrainedView({
+    Key? key,
+    required this.child,
+    this.width = 250,
+  }) : super(key: key);
+
+  final Widget child;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints){
+        if (constraints.maxWidth < width) {
+          return const Text (' ');
+        }
+        return child;
+      },
+    );
+  }
+}
+
