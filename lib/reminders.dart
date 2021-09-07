10d52037cc8cdcbea636b23f5929d43cba653cfe
Akhil Kasamsetty <awesomesetty99@gmail.com>
2021-09-06 23:16:32 -0500

    fixed reminders.dart

--- lib/reminders.dart
+++ lib/reminders.dart
@@ -1,10 +1,233 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'task.dart';
 
 class Reminders extends StatefulWidget {
   @override
   _RemindersState createState() => _RemindersState();
 }
-class HeroDialogRoute<T> extends PageRoute<T> {
+
+class _RemindersState extends State<Reminders> {
+  var _taskController;
+  late List<Task> _tasks;
+  late List<bool> _tasksDone;
+
+  void saveData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Task t = Task.fromString(_taskController.text);
+    String? tasks = prefs.getString('task');
+    List list = (tasks == null) ? [] : json.decode(tasks);
+    print(list);
+    list.add(json.encode(t.getMap()));
+    print(list);
+    prefs.setString('task', json.encode(list));
+    _taskController.text = '';
+    Navigator.of(context).pop();
+
+    _getTasks();
+  }
+
+  void _getTasks() async {
+    _tasks = [];
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tasks = prefs.getString('task');
+    List list = (tasks == null) ? [] : json.decode(tasks);
+    for (dynamic d in list) {
+      _tasks.add(Task.fromMap(json.decode(d)));
+    }
+
+    print(_tasks);
+
+    _tasksDone = List.generate(_tasks.length, (index) => false);
+    setState(() {});
+  }
+
+  void updatePendingTasksList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<Task> pendingList = [];
+    for (var i = 0; i < _tasks.length; i++)
+      if (!_tasksDone[i]) pendingList.add(_tasks[i]);
+
+    var pendingListEncoded = List.generate(
+        pendingList.length, (i) => json.encode(pendingList[i].getMap()));
+
+    prefs.setString('task', json.encode(pendingListEncoded));
+
+    _getTasks();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _taskController = TextEditingController();
+
+    _getTasks();
+  }
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Task Manager',
+          style: GoogleFonts.montserrat(),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: updatePendingTasksList,
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.setString('task', json.encode([]));
+
+              _getTasks();
+            },
+          ),
+        ],
+      ),
+      body: (_tasks == null)
+          ? Center(
+        child: Text('No Tasks added yet!'),
+      )
+          : Column(
+        children: _tasks
+            .map((e) => Container(
+          height: 70.0,
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+            vertical: 5.0,
+          ),
+          padding: const EdgeInsets.only(left: 10.0),
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            border: Border.all(
+              color: Colors.black,
+              width: 0.5,
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                e.task,
+                style: GoogleFonts.montserrat(),
+              ),
+              Checkbox(
+                value: _tasksDone[_tasks.indexOf(e)],
+                key: GlobalKey(),
+                onChanged: (val) {
+                  setState(() {
+                    _tasksDone[_tasks.indexOf(e)] = val!;
+                  });
+                },
+              ),
+            ],
+          ),
+        ))
+            .toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blue,
+        onPressed: () => showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) => Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 250,
+            color: Colors.blue[200],
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Add task',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: Icon(Icons.close),
+                    ),
+                  ],
+                ),
+                Divider(thickness: 1.2),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: _taskController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Enter task',
+                    hintStyle: GoogleFonts.montserrat(),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  width: MediaQuery.of(context).size.width,
+                  // height: 200.0,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: (MediaQuery.of(context).size.width / 2) - 20,
+                        child: RaisedButton(
+                          color: Colors.white,
+                          child: Text(
+                            'RESET',
+                            style: GoogleFonts.montserrat(),
+                          ),
+                          onPressed: () => _taskController.text = '',
+                        ),
+                      ),
+                      Container(
+                        width: (MediaQuery.of(context).size.width / 2) - 20,
+                        child: RaisedButton(
+                          color: Colors.blue,
+                          child: Text(
+                            'ADD',
+                            style: GoogleFonts.montserrat(),
+                          ),
+                          onPressed: () => saveData(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*class HeroDialogRoute<T> extends PageRoute<T> {
   /// {@macro hero_dialog_route}
   HeroDialogRoute({
     required WidgetBuilder builder,
@@ -44,9 +267,9 @@ class HeroDialogRoute<T> extends PageRoute<T> {
 
   @override
   String get barrierLabel => 'Popup dialog open';
-}
+}*/
 
-abstract class AppColors {
+/*abstract class AppColors {
   /// Dark background color.
   static const Color backgroundColor = Color(0xFF191D1F);
 
@@ -236,4 +459,4 @@ class ConstrainedView extends StatelessWidget {
     );
   }
 }
-
+*/
