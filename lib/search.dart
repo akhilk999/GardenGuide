795262a5d108eaf837168bb41f9e4436e1d3e734
Akhil Kasamsetty <awesomesetty99@gmail.com>
2021-10-23 01:00:39 -0500

    added search bar in search.dart

--- lib/search.dart
+++ lib/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  late String search;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +21,32 @@ class _SearchState extends State<Search> {
             style: TextStyle(fontSize: 20,color: Colors.grey[200],)),
         backgroundColor: Colors.green[600],
       ),
-      body: Center(
-        child: Text('Hell'),
+      body: Column(
+        children: <Widget> [
+          Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 25),
+                SizedBox(
+                width: 350,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    search = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    icon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+              ],
+            ),
+          ),
+        ]
       ),
     );
   }
