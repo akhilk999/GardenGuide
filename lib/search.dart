8878ded86821131e5c5934b0023ef1a266497f15
saahil <saahilm30@gmail.com>
2021-10-31 19:11:05 -0500

    Added images to search.dart

--- lib/search.dart
+++ lib/search.dart
@@ -108,12 +108,14 @@ class _SearchState extends State<Search> {
                                       region == "Tennessee" || region == "Kentucky" || region == "Florida") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => southernRegion()));
+                                        Image.asset('https://media.istockphoto.com/photos/lobelia-cardinalis-flowers-picture-id1277201249?k=20&m=1277201249&s=612x612&w=0&h=92KgIjJMpCnZwWvXmFJor0q-gevIK-d_mv6mNeVc5ss=', scale: 12,);
                                   }}
                                 if (_formKey.currentState!.validate()) {
                                   if (region == "Maine" || region == "New Hampshire" || region == "Vermont" || region == "Massachusetts" ||
                                       region == "Rhode Island" || region == "Connecticut") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/newEnglandRegion"),builder: (context) => newEnglandRegion()));
+                                        Image.asset('https://media.istockphoto.com/photos/silverspotted-skipper-feeding-on-new-england-aster-in-late-summers-picture-id1338957998?k=20&m=1338957998&s=612x612&w=0&h=x9ZCQv_pSnPqhWRiVUMUdafPwC9mot9Jf8bUb80tnbo=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
@@ -121,6 +123,7 @@ class _SearchState extends State<Search> {
                                       region == "Maryland" || region == "Virginia" || region == "West Virginia" || region == "Ohio") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => midAtlanticRegion()));
+                                    Image.asset('https://media.istockphoto.com/photos/foxglove-beardtongue-picture-id1324897251?k=20&m=1324897251&s=612x612&w=0&h=bl8Ldffl4_QuPZ4akK8ZoPaloUp4BHWMcY7Uxfm_5do=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
@@ -128,6 +131,7 @@ class _SearchState extends State<Search> {
                                       region == "Michigan" || region == "Minnesota" || region == "Missouri" || region == "Nebraska") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => midwestRegion()));
+                                    Image.asset('https://media.istockphoto.com/photos/flying-hummingbird-picture-id1331312490?b=1&k=20&m=1331312490&s=170667a&w=0&h=VajxZpvX2sj3qNDSrIQM3oxpPFwrtKIjtxxH0Fc8hAI=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
@@ -135,18 +139,21 @@ class _SearchState extends State<Search> {
                                       region == "South Dakota" || region == "Texas" || region == "Wyoming") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => southwestRegion()));
+                                    Image.asset('https://media.istockphoto.com/photos/plains-blackfoot-picture-id480154168?k=20&m=480154168&s=612x612&w=0&h=bSOUJqmTwFwxOeuKzZyHyHy86_YAaEVAa_A88UFjbgw=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
                                   if (region == "California" || region == "Oregon" || region == "Nevada" || region == "Arizona" || region == "Washington" || region == "Idaho" || region == "Utah") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => rockyMountains()));
+                                    Image.asset('https://media.istockphoto.com/photos/shrub-of-potentilla-fruticosa-sommerflor-shrubby-cinquefoil-yellow-picture-id1293902515?b=1&k=20&m=1293902515&s=170667a&w=0&h=Zvx6pdVH1mq1phmrw9KK9ChKua-7R07Z8jTa8IChkNc=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
                                   if (region == "Hawaii" || region == "Alaska") {
                                     Navigator.push(
                                         context, MaterialPageRoute(settings: RouteSettings(name: "/southernRegion"),builder: (context) => pacificCoastalRegion()));
+                                    Image.asset('https://media.istockphoto.com/photos/close-up-of-california-poppies-during-peak-blooming-time-antelope-picture-id1092780354?k=20&m=1092780354&s=612x612&w=0&h=Mj8BN2rnyohzTKL9fYQ2ae12JZ9TbPyMn8Jjtyi-6vE=', scale: 12,);
                                   }
                                 }
                                 if (_formKey.currentState!.validate()) {
